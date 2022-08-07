b750491cd2357bda9f37d38c3c9396c0a22ff43c
KhoaNA <khoa.na1266@sinhvien.hoasen.edu.vn>
2022-08-07 19:22:53 +0700

    5_8_22_AngularReduxEffects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { NbMenuItem, NbSidebarService } from '@nebular/theme';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularEffect';
+  constructor(private sidebarService: NbSidebarService) {
+  }
+  items: NbMenuItem[] = [
+    {
+      title: 'Home',
+      icon: 'home',
+      link: '/home'
+    },
+    {
+      title: 'Logout',
+      icon: 'unlock-outline',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
+import { articlesReducer } from './reducers/articles.reducer';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { MatCardModule } from '@angular/material/card';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbMenuModule, NbButton, NbButtonModule, NbInputModule, NbSearchModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HttpClientModule} from '@angular/common/http'
+import { ArticlesEffects } from './effects/articles.effects';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    EffectsModule.forRoot([
+      ArticlesEffects
+    ]),
+    StoreModule.forRoot({
+      articles: articlesReducer
+    }, {}),
+    MatCardModule,
+    MatSliderModule,
+    MatGridListModule,
+    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbButtonModule,
+    NbInputModule,
+    NbSearchModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
